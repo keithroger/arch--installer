4dbceec2011002dab67d39e88e8654beb51cae29
keith <keithontiveros@ucla.edu>
2022-07-18 14:29:28 -0700

    Update chroot

--- arch-install.sh
+++ arch-install.sh
@@ -2,6 +2,9 @@
 
 #TODO use sed to change echo commands to colored output
 
+# TODO use the arch-chroot one line implimentation
+# TODO put chroot items all in one script
+
 DISK="/dev/nvme0n1"
 EFI="/dev/nvme0n1p1"
 LFS="/dev/nvme0n1p2"
@@ -44,39 +47,36 @@ echo "Generating fstab"
 genfstab -U -p /mnt >> /mnt/etc/fstab
 cat /mnt/etc/fstab
 
-echo "Changing root to /mnt"
-arch-chroot /mnt
-
 echo "Adding host"
-echo "$hostname" > /etc/hostname
-cat > /etc/hosts <<EOF
+echo "$hostname" > /mnt/etc/hostname
+cat > /mnt/etc/hosts <<EOF
 127.0.0.1   localhost
 ::1         localhost
 127.0.1.1   $hostname.localdomain   $hostname
 EOF
 
 echo "Setup local timezone."
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
+arch-chroot /mnt locale-gen
+echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 
-export LANG=en_US.UTF-8
+# export LANG=en_US.UTF-8
 
-ln -s /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
-hwclock --systohc 
+arch-chroot /mnt ln -s /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+arch-chroot /mnt hwclock --systohc 
 
 echo "Setup users."
-echo "$password" | passwd
-useradd -m -G wheel $username
-echo $password | passwd $username
-echo '%wheel ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo
+arch-chroot /mnt echo "$password" | passwd
+arch-chroot /mnt useradd -m -G wheel $username
+arch-chroot /mnt echo $password | passwd $username
+arch-chroot /mnt echo '%wheel ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo
 
 echo "Setting up GRUB"
-mkdir /boot/efi
-mount /dev/sda1 /boot/efi
-grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck --removable
-grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt mkdir /boot/efi
+arch-chroot /mnt mount $EFI /boot/efi
+arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck --removable
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "Enabling Network Manager"
-systemctl enable NetworkManager
+arch-chroot /mnt systemctl enable NetworkManager
 
