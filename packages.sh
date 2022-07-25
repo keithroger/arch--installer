a2721095508b85239bc3d5e345d8dace75139494
Keith Ontiveros <keithontiveros@ucla.edu>
2022-07-25 14:20:00 -0700

    Update packages

--- packages.sh
+++ packages.sh
@@ -6,7 +6,7 @@ pacman -S git firefox htop ranger tree thunar rofi zathura zathura-pdf-mupdf  \
     xorg-xmodmap xorg-xbacklight python-pipx unzip lxappearance-gtk3 sysstat \
     ueberzug unclutter xorg xorg-xinit openssh ttf-fira-code bc libinput openssh \
     base-devel alsa-utils pulseaudio acpilight xf86-input-synaptics ripgrep neovim \
-    acpi
+    acpi npm gopls
 
 echo ".cfg" > /mnt/home/kro/.gitignore
 git clone --bare git@github.com:keithroger/dotfiles.git $HOME/.cfg
@@ -17,3 +17,4 @@ cp /home/kro/.config/X11/xorg.conf.d/30-touchpad.conf
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+npm i -g bash-language-server
