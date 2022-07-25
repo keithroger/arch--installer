439b446a1ae6cd421e9f484cd592209773de4912
Keith Ontiveros <keithontiveros@ucla.edu>
2022-07-25 13:43:02 -0700

    Update signing keys

--- packages.sh
+++ packages.sh
@@ -1,9 +1,16 @@
-pacstrap /mnt git firefox htop ranger tree thunar rofi zathura zathura-pdf-mupdf  \
+# TODO install yay
+
+pacman -Sy archlinux-keyring && pacman -Su
+
+pacman -S git firefox htop ranger tree thunar rofi zathura zathura-pdf-mupdf  \
     xorg-xmodmap xorg-xbacklight python-pipx unzip lxappearance-gtk3 sysstat \
     ueberzug unclutter xorg xorg-xinit openssh ttf-fira-code bc libinput openssh \
-    base-devel alsa-utils pulseaudio acpilight xf86-input-synaptics ripgrep neovim
+    base-devel alsa-utils pulseaudio acpilight xf86-input-synaptics ripgrep neovim \
+    acpi
 
 echo ".cfg" > /mnt/home/kro/.gitignore
-arch-chroot /mnt git clone --bare git@github.com:keithroger/dotfiles.git $HOME/.cfg
+git clone --bare git@github.com:keithroger/dotfiles.git $HOME/.cfg
 
-arch-chroot /mnt cp /home/kro/.config/X11/xorg.conf.d/30-touchpad.conf
+cp /home/kro/.config/X11/xorg.conf.d/30-touchpad.conf
+
+
