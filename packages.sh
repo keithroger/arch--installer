506523b2066d6c0abfb9730be788c08370b84299
Keith Ontiveros <keithontiveros@ucla.edu>
2022-07-25 14:20:33 -0700

    Add xclip

--- packages.sh
+++ packages.sh
@@ -6,7 +6,7 @@ pacman -S git firefox htop ranger tree thunar rofi zathura zathura-pdf-mupdf  \
     xorg-xmodmap xorg-xbacklight python-pipx unzip lxappearance-gtk3 sysstat \
     ueberzug unclutter xorg xorg-xinit openssh ttf-fira-code bc libinput openssh \
     base-devel alsa-utils pulseaudio acpilight xf86-input-synaptics ripgrep neovim \
-    acpi npm gopls
+    acpi npm gopls xclip
 
 echo ".cfg" > /mnt/home/kro/.gitignore
 git clone --bare git@github.com:keithroger/dotfiles.git $HOME/.cfg
